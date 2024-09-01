bee620a19059628e0aec95798e9748333bc02d43
thiagorabaioli <tfrabaioli@outlook.com>
2024-09-01 08:09:38 +0100

    first commit

---
+++ backend/src/main/java/thiagorabaioli/laf/LafApplication.java
@@ -0,0 +1,13 @@
+package thiagorabaioli.laf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/thiagorabaioli/laf/LafApplicationTests.java
@@ -0,0 +1,13 @@
+package thiagorabaioli.laf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
